e23b912795de9c7e9344a902790addf053eeb0db
g1tka <luckycookiedaisukii@gmail.com>
2024-07-30 08:11:28 +0000

    Finish JS-3

---
+++ JS-1,2/app.js
@@ -0,0 +1,44 @@
+let hello = "Hello World";
+// alert('Hello World');
+// これは消します。
+// alert('hello')
+// これはまちがえました。"hello"が表示されます。
+// alert(hello);
+
+// たとえば、整数を代入するときは、以下のように記述します。変数名は、ここではint1としています。
+// let int1 = 1;
+// alert(int1);
+// 「1」が表示されます。
+
+// 整数を代入する
+// let int1 = 1;
+
+// 負数を代入する
+let int2 = -10;
+
+// 小数点を代入する
+let float1 = 3.14;
+
+// 文字列を代入する
+// let str1 = 'JavaScriptを覚えよう';
+alert(int2)
+alert(float1);
+// alert(str1);
+
+// 足し算
+alert(2 + 15);
+
+// 引き算
+alert(18 - 4);
+
+// 掛け算
+alert(2 * 10);
+
+// 割り算
+alert(10 / 3);
+
+alert('Hello' + 'World');
+
+let str1 = 'Hello';
+let str2 = 'World!!';
+alert(str1 + str2);
--- app.js
+++ app.js
@@ -1,44 +1,104 @@
-let hello = "Hello World";
-// alert('Hello World');
-// これは消します。
-// alert('hello')
-// これはまちがえました。"hello"が表示されます。
-// alert(hello);
+// function 関数名(引数, 引数, 引数…){
+//   処理
+//   return 戻り値;
+// }
+// では、関数を書いてみる。（これだけでは"無"）
+// function addString(strA){
+//   let addStr = "Hello " + strA;
+//   return addStr;
+// }
 
-// たとえば、整数を代入するときは、以下のように記述します。変数名は、ここではint1としています。
-// let int1 = 1;
-// alert(int1);
-// 「1」が表示されます。
+// // メイン部分
+// let alertString;
 
-// 整数を代入する
-// let int1 = 1;
+// // 作成した関数を呼び出し、変数へ格納
+// alertString = addString("WebCamp");
 
-// 負数を代入する
-let int2 = -10;
+// //変数の中身をアラートで表示する
+// alert(alertString);
 
-// 小数点を代入する
-let float1 = 3.14;
+// 作成した関数
+// function addString(strA){
+//   let addStr = "Hello " + strA;
+//   return addStr;
+// }
 
-// 文字列を代入する
-// let str1 = 'JavaScriptを覚えよう';
-alert(int2)
-alert(float1);
-// alert(str1);
+// > Hello WebCampと出力される。
 
-// 足し算
-alert(2 + 15);
+// 複数の関数で表示
+// let promptStr = prompt('何か好きな文字を入力してください。');
 
-// 引き算
-alert(18 - 4);
+// alert(promptStr);
+// 入力された文字列はprompt()関数の戻り値として変数「promptStr」に格納され、アラートウィンドウ上に表示される
 
-// 掛け算
-alert(2 * 10);
+// じゃんけんの手を入力してもらうプロンプト欄を生成
+// prompt() を呼び出すと、ブラウザがポップアップダイアログボックスを表示する。
+let user_hand = prompt('じゃんけんの手をグー、チョキ、パーから選んでください。');
+// グーじゃない　かつ　チョキじゃない　かつ　パーじゃない　かつ　キャンセルじゃない　のときは
+while ((user_hand != "グー") && (user_hand != "チョキ") && (user_hand != "パー") && (user_hand != null)){
+  alert('グー・チョキ・パーのいずれかを入力して下さい');
+  user_hand = prompt('じゃんけんの手をグー、チョキ、パーから選んでください。');
+}
+// じゃんけんの手をランダムに作成する関数を呼び出す
+let js_hand = getJShand();
 
-// 割り算
-alert(10 / 3);
+// ユーザの手とJavaScriptのじゃんけんの手を比べる関数を呼び出し、結果をjudgeに入れる
+let judge = winLose(user_hand, js_hand);
 
-alert('Hello' + 'World');
+// 結果を表示する。キャンセル押すとuser_handがnullとなる。これを回避したい。
+if (user_hand != null){
+  alert('あなたの選んだ手は' + user_hand + 'です。\nJavaScriptの選んだ手は' + js_hand + 'です。\n結果は' + judge + 'です。');
+} else {
+  alert("またチャレンジしてね")
+}
+// 
 
-let str1 = 'Hello';
-let str2 = 'World!!';
-alert(str1 + str2);
+// ランダムでじゃんけんの手を作成する関数
+function getJShand(){
+  let js_hand_num = Math.floor( Math.random() * 3 );
+  // 0~2までのランダムな整数値を生成する。（3個の数字）
+  let hand_name;
+
+  if(js_hand_num == 0){
+    hand_name = "グー";
+  } else if(js_hand_num == 1){
+    hand_name = "チョキ";
+  } else if(js_hand_num == 2){
+    hand_name = "パー";
+  }
+
+  return hand_name;
+}
+
+// ユーザの手とJavaScriptのじゃんけんの手を比べる関数
+function winLose(user, js){
+  let winLoseStr;
+
+  if(user == "グー"){
+    if(js == "グー"){
+      winLoseStr = "あいこ";
+    } else if(js == "チョキ"){
+      winLoseStr = "勝ち";
+    } else if(js == "パー"){
+      winLoseStr = "負け";
+    }
+  } else if(user == "チョキ"){
+    if(js == "グー"){
+      winLoseStr = "負け";
+    } else if(js == "チョキ"){
+      winLoseStr = "あいこ";
+    } else if(js == "パー"){
+      winLoseStr = "勝ち";
+    }
+  } else if(user == "パー"){
+    if(js == "グー"){
+      winLoseStr = "勝ち";
+    } else if(js == "チョキ"){
+      winLoseStr = "負け";
+    } else if(js == "パー"){
+      winLoseStr = "あいこ";
+    }
+  }
+
+  return winLoseStr;
+}
