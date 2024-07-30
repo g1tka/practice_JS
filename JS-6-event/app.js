7f756b9484081d67b52650d98f7f56e9284f4dea
g1tka <luckycookiedaisukii@gmail.com>
2024-07-30 11:29:23 +0000

    Finish JS-6

---
+++ JS-6-event/app.js
@@ -0,0 +1,19 @@
+$(function(){
+  $('.box1').mouseover(function(){
+    // $('.box1').css({'background-color': '#0000FF'});
+    $('.box1').addClass('box1-ext');
+  });
+  $('.box1').mouseout(function(){
+    // $('.box1').css({'background-color': '#FF0000'});
+    $('.box1').removeClass('box1-ext');
+  });
+});
+
+$(function(){
+  $('.box2').on('click', function(){
+    $('.box2').addClass('box2-ext');
+  });
+  $('.box2').mouseout(function(){
+    $('.box2').removeClass('box2-ext');
+  });
+});
