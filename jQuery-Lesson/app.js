300900a9efce04b6b2cc356c97eeba0537524c58
g1tka <luckycookiedaisukii@gmail.com>
2024-07-30 10:01:12 +0000

    Finish JS-4

---
+++ jQuery-Lesson/app.js
@@ -0,0 +1,16 @@
+$(document).ready(function () {
+  $('body').html('<h1>Hello jQuery!!</h1>');
+  // $( セレクタ ).メソッド ( パラメータ );
+});
+
+// bodyタグ内のHTMLを、<h1>Hello JQuery!!</h1>で書き換えなさい。
+
+// 基本の書き方
+// $(document).ready(function () {
+//   // jQueryプログラムの内容
+// });
+// もしくは省略形
+// $(function(){
+//   // jQueryプログラムの内容
+// });
+
